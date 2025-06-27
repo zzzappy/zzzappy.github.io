4d12c8185bd1eed435f0a76c793b6b42f622ceee
zzzappy <90869312+zzzappy@users.noreply.github.com>
2025-06-26 23:54:07 -0700

    CI/CD: Edit deploy.yml so build happens on PR, deploy only on merge/push to main

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Sam Zappa</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
