7a10e50476da6cba0246cb53257fe746328829f6
zzzappy <90869312+zzzappy@users.noreply.github.com>
2025-06-28 21:38:31 -0700

    Add purpose statement, make footer/typewriter text adapt better to screen size

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,18 @@ function App() {
     <div className="main-content">
       {/* animating svg: https://maxwellito.github.io/vivus-instant/ */}
       <img
-        src={
-          // Prevents caching so svg animates on each load
-          `/name_animated_no_loop.svg?${Date.now()}`
-        }
+        src={`/name_animated_no_loop.svg?${Date.now()}`}
         alt="Sam Zappa"
         className="animated-name-img slide-fade-in"
       />
+      <div className="purpose-statement">
+        <div className="purpose-main">
+          Computer Science student at the University of Washington Paul G. Allen School of Computer Science & Engineering. Motivated and passionate about software engineering, problem solving, and building impactful technology.
+        </div>
+        <div className="purpose-objective">
+          Seeking internship or entry-level software engineering opportunities where I can apply my strong analytical and collaborative skills. Looking for a role where I can contribute to an organization's goals while gaining valuable life and work experience.
+        </div>
+      </div>
       <div className="social-links tech-font">
         <a href="https://www.linkedin.com/in/sam-zappa-237922276/" target="_blank" rel="noopener" className="social-btn linkedin-btn">
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="LinkedIn" /> LinkedIn
@@ -29,7 +34,7 @@ function App() {
             window.location.href = 'mailto:' + ['spzappa.personal', 'gmail.com'].join('@');
           }}
         >
-          <MailIcon fontSize='large'/>
+          <MailIcon className="email-icon" />
           Contact
         </a>
       </div>
